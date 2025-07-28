8f2d4b79d9581ca09cee10d15627fd6b0b04ee36
LittleNyima <littlenyima@163.com>
2025-07-29 01:30:27 +0800

    introduce react-router-dom

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App: React.FC = () => {
   return (
-    <>
-      <div>Hello, world!</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Hello, navrim!</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
